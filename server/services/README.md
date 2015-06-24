85977544b37b0171f73515e5448b7c7fc88e0bc5
Marshall Clyburn <marshall@mardev.net>
2015-06-23 23:17:56 -0400

    Add full backup service.

--- server/services/README.md
+++ server/services/README.md
@@ -3,7 +3,13 @@ Services
 
 My server's custom systemd services.
 
+full-backup
+-----------
+
+Create a compressed, tar backup of the computer.
+
 zfs-report
 ----------
 
 Generate a report about the status of ZFS and post it in the web directory.
+
---
+++ server/services/full-backup/backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Create a tar backup of the entire system.
+
+if test -z $1
+then
+	printf "Usage: backup <destination>\n"
+	exit 1
+fi
+
+# ensure we have superuser privileges.
+if test $(id -u) != "0"
+then
+	printf "Need superuser privileges.\n"
+	exit 1
+fi
+
+# set name
+backup_name="$(uname -r)-$(date +'%Y%m%d_%H%M%S')"
+printf "Backing up system. (${backup_name})\n"
+
+tar -c --exclude={'${1}/${backup_name}.tar','dev/*','home/*','media/*','mnt/*','proc/*','run/*','srv/btsync/*','srv/syncthing/*','sys/*','tmp/*','usr/portage/*','usr/src/*','var/tmp/*'} -Ipixz -f ${1}/${backup_name}.tar.pxz -C / .
+
+exit 0
+
